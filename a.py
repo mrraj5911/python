0b72596743dcc0f1ddac2de89cad291457b7aa71
raj <ranajiagd6788@gmail.com>
2021-08-28 13:00:39 +0530

    python files

---
+++ a.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number "))
+i=1
+sum=0
+while i<=a:
+    sum+=i
+    i+=1
+print("the sum of first {} numbers is {}".format(a,sum))
---
+++ p.py
@@ -0,0 +1,6 @@
+name=""
+pwd=''
+while (name!='raj') or (pwd!="rana"):
+    name=input("enter the name: ")
+    pwd=input("enter the password: ")
+print("hello sir")
